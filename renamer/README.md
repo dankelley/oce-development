1a995055bbfa3927066d2e283c154e534caed7fc
dankelley <kelley.dan@gmail.com>
2024-09-15 08:02:51 -0300

    NEXT: other names in local files, bodc from python

---
+++ renamer/README.md
@@ -0,0 +1,3 @@
+See cioos_data_transform/cioos_data_transform/utils/bodc_generator.csv in
+cioos-siooc_data_transform github project for conversion between conventional
+type long names to these ioss names.
--- renamer/renamer01.R
+++ renamer/renamer01.R
@@ -1,19 +1,20 @@
 library(oce)
 # NB ctd.sbe.R:39 is wrong; no such function exists (yet)
-#vocab <- read.csv("sbe.csv",
+# vocab <- read.csv("sbe.csv",
 #    header = FALSE,
 #    col.names = c("original", "oce", "unit", "scale")
-#)
+# )
+
 # The system is that ~ can represent a digit
-#vocab$pattern <- paste0("^", gsub("~", "[0-9]", vocab$original), "$")
-#if (debug > 0) print(vocab)
+# vocab$pattern <- paste0("^", gsub("~", "[0-9]", vocab$original), "$")
+# if (debug > 0) print(vocab)
 
 #' Rename variables according to a specified dictionary
-renamer <- function(s, dictionary="sbe.csv", debug = 0) {
+renamerTest1 <- function(names, dictionary = "sbe.csv", debug = 0) {
     debug <- min(3L, max(debug, 0L))
     if (is.character(dictionary) && grepl(".csv$", dictionary)) {
-        oceDebug(debug, "renamer() reading dictionary file \"", dictionary, "\"\n", sep = "")
-        vocab <- read.csv("sbe.csv",
+        oceDebug(debug, "renamerTest1() reading dictionary file \"", dictionary, "\"\n", sep = "")
+        vocab <- read.csv(dictionary,
             header = FALSE,
             col.names = c("original", "oce", "unit", "scale")
         )
@@ -23,35 +24,46 @@ renamer <- function(s, dictionary="sbe.csv", debug = 0) {
     if (debug > 1) {
         print(vocab)
     }
-    # Look up
-    # FIXME: permit multiple names (to avoid reading the file over and over)
-    oceDebug(debug, "searching for an exact name match to \"", s, "\"\n")
-    w <- which(s == vocab$original)
-    if (length(w) == 0L) {
-        oceDebug(debug, "grepping for a pattern match\n")
-        w <- which(sapply(vocab$pattern, \(pattern) grepl(pattern, s)))
-        oceDebug(debug, vectorShow(w))
-        if (length(w) > 1L) {
-            warning("multiple name match on \"", s, "\"", sep = "")
-            w <- NULL
+    rval <- vector("list", length(names))
+    for (i in seq_along(names)) {
+        name <- names[i]
+        # Look up
+        oceDebug(debug, "searching for an exact name match to \"", name, "\"\n")
+        w <- which(name == vocab$original)
+        if (length(w) == 0L) {
+            oceDebug(debug, "no exact match, so searching patterns for a match\n")
+            w <- which(sapply(vocab$pattern, \(pattern) grepl(pattern, name)))
+            oceDebug(debug, vectorShow(w))
+            if (length(w) > 1L) {
+                warning("multiple name match on \"", name, "\"", sep = "")
+                w <- NULL
+            }
         }
-    }
-    if (length(w) == 0L) {
-        rval <- list(name = s, unit = "")
-    } else {
-        oceDebug(debug, "this name matches vocabulary item at index w=", w, "\n")
-        oceDebug(debug, vectorShow(vocab[w, ]))
-        unit <- try(parse(text = vocab$unit[w]), silent = TRUE)
-        rval <- if (inherits(unit, "try-error")) {
-            list(name = vocab$oce[w], unit = vocab$unit[w])
+        if (length(w) == 0L) {
+            rval[[i]] <- list(name = name, unit = "", scale = "")
         } else {
-            list(name = vocab$oce[w], unit = parse(text = vocab$unit[w]))
+            oceDebug(debug, "this name matches vocabulary item at index w=", w, "\n")
+            oceDebug(debug-1, vectorShow(vocab[w, ]))
+            unit <- try(parse(text = vocab$unit[w]), silent = TRUE)
+            rval[[i]] <- if (inherits(unit, "try-error")) {
+                list(name = vocab$oce[w], unit = vocab$unit[w], scale = vocab$scale[w])
+            } else {
+                list(name = vocab$oce[w], unit = parse(text = vocab$unit[w]), scale = vocab$scale[w])
+            }
         }
     }
-    rval # FIXME: allow multiples
+    rval
 }
-renamer("accM", "sbe.csv", debug = 1)
-# renamer("t090C")
-# renamer("cond0S/m")
-renamer("oxsatMg/L", debug = 1)
-# O <- renamer("density")
+
+# Test 1
+newNamesAndUnits <- renamerTest1(c("accM", "oxsatMg/L"), "sbe.csv", debug = 0)
+stopifnot(all.equal(newNamesAndUnits[[1]]$name, "acceleration"))
+stopifnot(all.equal(newNamesAndUnits[[2]]$name, "oxygen"))
+str(newNamesAndUnits, give.attr = FALSE)
+
+d <- read.netcdf("CTD_CAR2023011_001_496780_DN.ODF.nc")
+oldNames <- names(d[["data"]])
+newNames <- renamerTest1(oldNames, "ioos.csv", debug=0)
+df <- data.frame(new=unname(sapply(newNames, \(x) x$name)), old=oldNames)
+df
+message("FIXME: Q values, OXYOCPVL01, IRRDUV01, CPHLPR01, AHSFZZ01, RecPerBin")
