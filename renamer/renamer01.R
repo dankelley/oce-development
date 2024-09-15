fd3aa77ef033f244c43423f2e2e12ebb5662a1c3
dankelley <kelley.dan@gmail.com>
2024-09-15 14:55:43 -0300

    return data.frame; handle approx 5 more variables

--- renamer/renamer01.R
+++ renamer/renamer01.R
@@ -21,15 +21,18 @@ renamerTest1 <- function(names, dictionary = "sbe.csv", debug = 0) {
     }
     # The system is that ~ can represent a digit
     vocab$pattern <- paste0("^", gsub("~", "[0-9]", vocab$original), "$")
+    print(vocab$pattern)
     if (debug > 1) {
         print(vocab)
     }
-    rval <- vector("list", length(names))
+    rval <- data.frame()
     for (i in seq_along(names)) {
         name <- names[i]
         # Look up
         oceDebug(debug, "searching for an exact name match to \"", name, "\"\n")
         w <- which(name == vocab$original)
+        print(w)
+        print(vocab[w, ])
         if (length(w) == 0L) {
             oceDebug(debug, "no exact match, so searching patterns for a match\n")
             w <- which(sapply(vocab$pattern, \(pattern) grepl(pattern, name)))
@@ -40,30 +43,38 @@ renamerTest1 <- function(names, dictionary = "sbe.csv", debug = 0) {
             }
         }
         if (length(w) == 0L) {
-            rval[[i]] <- list(name = name, unit = "", scale = "")
+            oceDebug(debug, "\"", name, "\" is not in vocabulary\n", sep = "")
+            rval <- rbind(rval, c(originalName = name, oceName = name, unit = "", scale = ""))
         } else {
-            oceDebug(debug, "this name matches vocabulary item at index w=", w, "\n")
-            oceDebug(debug-1, vectorShow(vocab[w, ]))
-            unit <- try(parse(text = vocab$unit[w]), silent = TRUE)
-            rval[[i]] <- if (inherits(unit, "try-error")) {
-                list(name = vocab$oce[w], unit = vocab$unit[w], scale = vocab$scale[w])
-            } else {
-                list(name = vocab$oce[w], unit = parse(text = vocab$unit[w]), scale = vocab$scale[w])
-            }
+            oceDebug(debug, "\"", name, "\" matches vocabulary at index ", w, "\n", sep = "")
+            oceDebug(debug - 1, vectorShow(vocab[w, ]))
+            rval <- rbind(rval, c(
+                originalName = name,
+                oceName = vocab$oce[w], unit = vocab$unit[w], scale = vocab$scale[w]
+            ))
+            # Leave this to later, so we can return a data frame here
+            # unit <- try(parse(text = vocab$unit[w]), silent = TRUE)
+            # rval[[i]] <- if (inherits(unit, "try-error")) {
+            #    list(name = vocab$oce[w], unit = vocab$unit[w], scale = vocab$scale[w])
+            # } else {
+            #    list(name = vocab$oce[w], unit = parse(text = vocab$unit[w]), scale = vocab$scale[w])
+            # }
         }
     }
+    colnames(rval) <- c("originalName", "oceName", "unit", "scale")
     rval
 }
 
 # Test 1
-newNamesAndUnits <- renamerTest1(c("accM", "oxsatMg/L"), "sbe.csv", debug = 0)
-stopifnot(all.equal(newNamesAndUnits[[1]]$name, "acceleration"))
-stopifnot(all.equal(newNamesAndUnits[[2]]$name, "oxygen"))
-str(newNamesAndUnits, give.attr = FALSE)
+test <- renamerTest1(c("accM", "oxsatMg/L"), "sbe.csv", debug = 1)
+stopifnot(all.equal(test$oceName, c("acceleration", "oxygen")))
 
 d <- read.netcdf("CTD_CAR2023011_001_496780_DN.ODF.nc")
 oldNames <- names(d[["data"]])
-newNames <- renamerTest1(oldNames, "ioos.csv", debug=0)
-df <- data.frame(new=unname(sapply(newNames, \(x) x$name)), old=oldNames)
-df
-message("FIXME: Q values, OXYOCPVL01, IRRDUV01, CPHLPR03, AHSFZZ01, RecPerBin")
+R <- renamerTest1(oldNames, "ioos.csv", debug = 0)
+newNames <- R$oceNames
+df <- data.frame(new = R$oceName, old = R$originalName, check = oldNames)
+stopifnot(all.equal(df$old, df$check))
+print(df[, 1:2])
+
+message("NEXT: test multiple files, ideally freshly downloaded)")
